801ff9dbce175ff068ea8892355e2e871bc93fe4
cyyier <52512369+cyyier@users.noreply.github.com>
2025-04-03 20:53:22 +0900

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,14 +3,7 @@ import {initializeApp} from 'firebase/app';
 import {getDatabase, ref, push} from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD9a311G9MkXIQF6UD1xTrBt6I-U123ozo',
-  authDomain: 'kakeibo-rumiso.firebaseapp.com',
-  databaseURL:
-    'https://kakeibo-rumiso-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'kakeibo-rumiso',
-  storageBucket: 'kakeibo-rumiso.appspot.com',
-  messagingSenderId: '647040789893',
-  appId: '1:647040789893:android:fc7c16640825ace51028ec',
+  【非公開】
 };
 
 // 初始化 Firebase 应用
